537d05a3bbb7f3e04d7ef3964d4659559404458f
Fabio Nascimento <nascimento.fab@gmail.com>
2025-10-06 23:23:07 -0300

    Ajuste no nome da applicação

--- src/main/java/com/curso/java/completo/springbootwebwervice/SpringBootWebServiceApplication.java
+++ src/main/java/com/curso/java/completo/springbootwebwervice/SpringBootWebServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootwebwerviceApplication {
+public class SpringBootWebServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootwebwerviceApplication.class, args);
+        SpringApplication.run(SpringBootWebServiceApplication.class, args);
     }
 
 }
