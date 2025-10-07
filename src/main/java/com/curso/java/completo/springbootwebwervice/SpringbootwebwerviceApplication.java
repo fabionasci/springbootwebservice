5f37172bd1633f56d03180f996b464c0156f9bef
Fabio Nascimento <nascimento.fab@gmail.com>
2025-10-06 23:11:06 -0300

    First commit

---
+++ src/main/java/com/curso/java/completo/springbootwebwervice/SpringbootwebwerviceApplication.java
@@ -0,0 +1,13 @@
+package com.curso.java.completo.springbootwebwervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebwerviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebwerviceApplication.class, args);
+    }
+
+}
