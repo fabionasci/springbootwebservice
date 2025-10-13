445bb4b9d259a4e6956036455017ccc7a170ffb9
Fabio Nascimento <nascimento.fab@gmail.com>
2025-10-12 22:45:54 -0300

    ManyToMany Product and Category implementation

--- src/main/java/com/curso/java/completo/springbootwebservice/entities/Category.java
+++ src/main/java/com/curso/java/completo/springbootwebservice/entities/Category.java
@@ -1,5 +1,6 @@
 package com.curso.java.completo.springbootwebservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> product = new HashSet<>();
 
 
