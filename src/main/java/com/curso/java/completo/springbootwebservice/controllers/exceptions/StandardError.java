2422840c3e2371b2722bcdbe16539a8640fd4091
Fabio Nascimento <nascimento.fab@gmail.com>
2025-10-21 20:50:09 -0300

    Exception handling - findById (date format)

--- src/main/java/com/curso/java/completo/springbootwebservice/controllers/exceptions/StandardError.java
+++ src/main/java/com/curso/java/completo/springbootwebservice/controllers/exceptions/StandardError.java
@@ -1,5 +1,7 @@
 package com.curso.java.completo.springbootwebservice.controllers.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -7,6 +9,7 @@ public class StandardError implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
     private int status;
     private String error;
