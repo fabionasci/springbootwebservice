646c3e328189d47be9ce4696c9cbced69110b7fc
Fabio Nascimento <nascimento.fab@gmail.com>
2025-11-01 21:23:41 -0300

    dev and aws profiles with variables

--- src/main/java/com/curso/java/completo/springbootwebservice/SpringBootWebServiceApplication.java
+++ src/main/java/com/curso/java/completo/springbootwebservice/SpringBootWebServiceApplication.java
@@ -2,9 +2,16 @@ package com.curso.java.completo.springbootwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebServiceApplication {
+public class SpringBootWebServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(SpringBootWebServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebServiceApplication.class, args);
